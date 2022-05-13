fa1132ba82ae2118c492d0a431efe53ee1a6b08a
moog2001 <moogforhire@gmail.com>
2022-05-13 19:07:34 +0800

    Added login_view

--- src/main/java/com/example/remigo/HelloApplication.java
+++ src/main/java/com/example/remigo/HelloApplication.java
@@ -13,8 +13,9 @@ import java.util.List;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
